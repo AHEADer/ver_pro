650e4a69876221733244f0d13833920ed59ee07e
Zhang Junda <jundazhang@hustunique.com>
2016-09-02 16:25:34 +0800

    Update led.v
    complete

--- led.v
+++ led.v
@@ -20,7 +20,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module led(
+module clock(
 input cp,reset,open,sec_add, min_add, hour_add,
 output [7:0] show_led,
 output [0:6] led_out,
@@ -42,27 +42,26 @@ output signal
     reg [0:6] what=0;
     reg [3:0] value=0;
     reg [20:0] cnt_scan=0;
-    reg [3:0] sec_a_cnt, sec_b_cnt, min_a_cnt, min_b_cnt, hour_a_cnt, hour_b_cnt=0;
-    reg [3:0] sec_a, sec_b, min_a, min_b, hour_a, hour_b = 0;
-    wire cp_sec, cp_min, cp_hour;
+    wire [3:0] sec_a_cnt, sec_b_cnt, min_a_cnt, min_b_cnt, hour_a_cnt, hour_b_cnt;
+    wire [4:0] hour_cnt;
+    reg [3:0] sec_a, sec_b, min_a, min_b, hour_a, hour_b;
+    wire cp_sec, cp_min, cp_hour, cp_slow;
     wire cp_sec_, cp_min_, cp_hour_;
+    wire c_out1, c_out2, sec_min, min_hour;
+    reg c_in = 1;
     
     dclk d1(cp, open, cp_sec_);
-    clock60 c61(cp_sec_, open, cp_min_);
-    clock60 c62(cp_min_, open, cp_hour_);
-    ring r1(cp_sec_, reset, open, signal);
     
     assign cp_sec = open?cp_sec_:sec_add;
-    assign cp_min = open?cp_min_:min_add;
-    assign cp_hour = open?cp_hour_:hour_add;
+    assign cp_min = open?sec_min:min_add;
+    assign cp_hour = open?min_hour:hour_add;
     assign show_led = which;
     assign led_out = what;
-    
+        
     always @(posedge cp)
     begin
     cnt_scan  = cnt_scan + 1;
-    
-    case(cnt_scan[20:18])
+    case(cnt_scan[18:16])
         3'b000:which <= 8'b11111110;
         3'b001:which <= 8'b11111101;
         3'b010:which <= 8'b11111011;
@@ -85,8 +84,6 @@ output signal
     8'b1111111:value<=0;
     endcase
     
-    //value = reset?0:value;
-    
     case(value)
     4'b0000: what <= zero;
     4'b0001: what <= one;
@@ -101,125 +98,60 @@ output signal
     default: what <= zero;
     endcase                        
     end
-    
-    /*always @(reset)
-    begin
-    sec_a_cnt = 0;
-    sec_b_cnt = 0;
-    min_a_cnt = 0;
-    min_b_cnt = 0;
-    hour_a_cnt = 0;
-    hour_b_cnt = 0;
-    end*/
      
-    always @(posedge cp_sec)
+    //module count__10(cin,clk,qout,cout,reset);
+    //module count__6(cin,clk,qout,cout,reset);
+    count__10 cten1(c_in, cp_sec,sec_a_cnt,cout1,reset);
+    count__6 csix1(c_in,cout1,sec_b_cnt,sec_min,reset);
+    
+    count__10 cten2(c_in, cp_min,min_a_cnt,cout2,reset);
+    count__6 csix2(c_in,cout2,min_b_cnt,min_hour,reset);
+    
+    count__24 c24(c_in,cp_hour,hour_cnt,reset);
+    ring r1(cp_min ,cp_sec , sec_a, sec_b, min_a, min_b, open, signal);
+            
+    always @(sec_a_cnt)
     begin
-        sec_a_cnt = sec_a_cnt + 1;
-    
-    if(sec_a_cnt == 10)
-    begin
-        sec_a_cnt = 0;
-        sec_b_cnt = sec_b_cnt + 1;
-    end    
-    
-    if(sec_b_cnt == 6)
-        sec_b_cnt = 0;
-    sec_a = sec_a_cnt;
-    sec_b = sec_b_cnt;           
-    end 
-    
-    always @(posedge cp_min)
-    begin
-        min_a_cnt = min_a_cnt + 1;
-    
-    if(min_a_cnt == 10)
-    begin
-        min_a_cnt = 0;
-        min_b_cnt = min_b_cnt + 1;
-    end    
-    
-    if(min_b_cnt == 6)
-        min_b_cnt = 0;
-        
-    min_a = min_a_cnt;
-    min_b = min_b_cnt;            
-    end    
-    
-    always @(posedge cp_hour)
-    begin
-        hour_a_cnt = hour_a_cnt + 1;
-    
-    if(hour_a_cnt == 10)
-    begin
-        hour_a_cnt = 0;
-        hour_b_cnt = hour_b_cnt + 1;
-    end    
-    
-    if(hour_b_cnt == 2)
-    if(hour_a_cnt == 4)
-    begin
-        hour_b_cnt = 0;
-        hour_a_cnt = 0;
+    sec_a <= sec_a_cnt;
     end
-        
-    hour_a = hour_a_cnt;
-    hour_b = hour_b_cnt;        
-    end    
+
+    always @(sec_b_cnt)
+    begin
+    sec_b <= sec_b_cnt;
+    end
+    
+    always @(min_a_cnt)
+    begin
+    min_a <= min_a_cnt;
+    end
+    
+    always @(min_b_cnt)
+    begin
+    min_b <= min_b_cnt;
+    end
+    
+    always @(hour_cnt)
+    begin
+    hour_a <= hour_cnt%10;
+    hour_b <= hour_cnt/10;
+    end
      
 endmodule
 
-module clock24(
-input cp24,
-output cp24o
-    );
-    reg [0:3] cnt = 0;
-    reg cp=0;
-    assign cp24o = cp;
-    always @(posedge cp24)
-    begin
-    cnt <= cnt+1'b1;
-    if (cnt >= 11)
-    //if (cnt >= 3)
-    begin
-    cp <= ~cp;
-    cnt <= 1'b0;
-    end
-    end
-endmodule
-
-module clock60(
-input cp60, open,
-output cp60o
-    );
-   reg [0:4] cnt = 0;
-   reg cp=1;
-   assign cp60o = cp;
-   always @(posedge cp60)
-       begin
-       if(open)
-       cnt <= cnt+1'b1;
-       if (cnt >= 29)
-       //if (cnt >= 3)
-       begin
-       cp <= ~cp;
-       cnt <= 1'b0;
-       end
-       end 
-endmodule
 
 module dclk(
 input cp, open,
 output cpo
     );
     reg [0:31] cnt = 0;
-    reg cpd=0;
+    reg cpd=1;
     assign cpo = cpd;
     always @(posedge cp)
     begin
     if(open)
     cnt <= cnt+1'b1;
-    //if (cnt >= 49999999)
-    if (cnt >= 49999)
+    if (cnt >= 49999999)
+    //if (cnt >= 49999)
     begin
     cpd <= ~cpd;
     cnt <= 1'b0;
@@ -228,29 +160,89 @@ output cpo
 endmodule
 
 module ring(
-input cp_ring,  //input: seconds
-reset, open,
+input cp_ring,  //input: hours/mins
+cp_sec, 
+input [3:0] sec_a, sec_b, min_a, min_b,
+input open,
 output signal
     );
     reg [0:5] count = 1'b0;
     reg r_signal = 0;
     
     assign signal = r_signal;
+        
+    always @(negedge cp_sec)
+    begin
+    if(open)
+    if(sec_a == 0 && sec_b == 0 && min_a ==0 && min_b == 0)
+        r_signal = 1;
     
-    always @(posedge cp_ring)
+    if(r_signal == 1)
     begin
-    if(reset)
+    count = count + 1;
+    if(count >= 5)
+    begin
     count = 0;
-    else if(open)
-    count = count + 1'b0;
-    if(count>=59)
-    begin
-    r_signal <= 1;
-    if(count >=64)
-    begin
-    count <= 0;
-    r_signal <= 0;
+    r_signal = 0;
     end
     end
+       
+        
     end
+    
+endmodule
+
+module count__10(cin,clk,qout,cout,reset);
+input cin,clk,reset;
+output cout;
+output [3:0] qout;
+reg [3:0] qout;
+always @(negedge clk or negedge reset)
+begin
+if(reset==1) qout=0;
+else if(cin)
+begin
+   if(qout[3:0]==9)
+   begin qout[3:0]=0;
+end
+   else qout[3:0]=qout[3:0]+1;
+end
+end
+assign cout=((qout[3:0]==9)&cin)?1:0;
+endmodule
+
+module count__6(cin,clk,qout,cout,reset);
+input cin,clk,reset;
+output cout;
+output [3:0] qout;
+reg [3:0] qout;
+always @(negedge clk or negedge reset)
+begin
+if(reset==1) qout=0;
+else if(cin)
+begin
+   if(qout[3:0]==5)
+   begin qout[3:0]=0;
+end
+   else qout[3:0]=qout[3:0]+1;
+end
+end
+assign cout=((qout[3:0]==5)&cin)?1:0;
+endmodule
+
+module count__24(cin,clk,qout,reset);
+input cin,clk,reset;
+output [4:0] qout;
+reg [4:0] qout;
+always @(posedge clk or negedge reset)
+begin
+if(reset==1) qout=0;
+else if(cin)
+begin
+   if(qout[4:0]==23)
+   begin qout[4:0]=0;
+end
+   else qout[4:0]=qout[4:0]+1;
+end
+end
 endmodule
